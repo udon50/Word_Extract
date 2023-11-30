74509e58a4b98ea5cd03fa760411f7bfd6735266
YUSUKE <yusuke5058@gmail.com>
2023-11-30 16:17:46 +0900

    test2

---
+++ Word_Extract.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const countWords = (filePath) => {
+    const text = fs.readFileSync(filePath, 'utf-8');
+    const words = text.toLowerCase().split('\r\n');
+    const wordCounts = {};
+    words.forEach((word) => {
+        wordCounts[word] = (wordCounts[word] || 0) + 1;
+    });
+    return wordCounts;//単語の出現回数を返す
+};
+
+const getDuplicateWords = (wordCounts) => {
+    return Object.keys(wordCounts).filter((word) => wordCounts[word] > 1);
+};//重複した単語を返す
+
+const dirPath = './textFiles'; // ディレクトリのパスを指定
+const files = fs.readdirSync(dirPath).filter((file) => path.extname(file) === '.txt');//ディレクトリ内のテキストファイルを取得
+
+let totalWordCounts = {};
+files.forEach((file) => {
+    const wordCounts = countWords(path.join(dirPath, file));
+    Object.keys(wordCounts).forEach((word) => {
+        totalWordCounts[word] = (totalWordCounts[word] || 0) + wordCounts[word];
+    });
+});
+
+const duplicateWords = getDuplicateWords(totalWordCounts);
+console.log(duplicateWords);
+
+/* fsモジュールとpathモジュールを使って、指定したディレクトリ内のテキストファイルの単語の出現回数を数えて、
+全てのファイルの単語の出現回数を合計して、重複した単語を抽出してconsole.logで出力する。 */
